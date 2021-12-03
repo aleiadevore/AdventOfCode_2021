d8d003340f6f19f11e9cde3638b86600ad1bd8c0
aleiadevore <2496@holbertonschool.com>
2021-12-03 09:45:39 -0600

    Prepping for day 2

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 Coding challenges from the [2021 Advent of Code](https://adventofcode.com/2021)
 
 Day 1: [Sonar Sweep](SonarSweep)
+Day 2: [Dive!](Dive)
--- SonarSweep/part2.py
+++ SonarSweep/part2.py
@@ -5,3 +5,31 @@
 # don't use dict. Check list[i] + list[i + 1] + list[i + 2], make sure all exist
 # comapare to prev
 
+#!/usr/bin/python3
+
+# declare count
+from typing import Counter
+
+
+count = 0
+
+# import input
+with open("input") as file_in:
+    lines = []
+    for line in file_in:
+        lines.append(line)
+
+# parse through lines and increment count if l2 > l1
+old = 999999
+current = 0
+for i in range(0, len(lines) - 1):
+    if (i + 2 < len(lines)):
+        current = int(lines[i]) + int(lines[i + 1]) + int(lines[i + 2])
+    else:
+        break
+    if current > old:
+        count += 1
+    old = current
+
+# print final answer
+print(count)
