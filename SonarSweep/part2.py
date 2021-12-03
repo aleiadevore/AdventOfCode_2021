9fcd549edaa646a509d50b9eb40c48acafce4233
aleiadevore <2496@holbertonschool.com>
2021-12-03 09:38:05 -0600

    Brainstorming process

---
+++ SonarSweep/part2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# use dict to += Letter: value
+# use letter instead of number so it can be incremented indefinitely
+# don't use dict. Check list[i] + list[i + 1] + list[i + 2], make sure all exist
+# comapare to prev
+
