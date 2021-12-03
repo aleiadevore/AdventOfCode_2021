923cd0ad6cba90101bfa89118e280e2a7a4455d2
aleiadevore <2496@holbertonschool.com>
2021-12-02 23:53:01 -0600

    Correct answer

--- SonarSweep/sonar.py
+++ SonarSweep/sonar.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 # declare count
+from typing import Counter
+
+
 count = 0
 
 # import input
@@ -10,5 +13,11 @@ with open("input") as file_in:
         lines.append(line)
 
 # parse through lines and increment count if l2 > l1
+old = 0
+for i in range(0, len(lines) - 1):
+    if int(lines[i]) > old:
+        count += 1
+    old = int(lines[i])
 
 # print final answer
+print(count)
