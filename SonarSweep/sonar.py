cadbcb1155649442c2d85d54a6d4ab99b8e0c7a4
aleiadevore <2496@holbertonschool.com>
2021-12-02 23:34:18 -0600

    Saving before sleep

---
+++ SonarSweep/sonar.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# declare count
+count = 0
+
+# import input
+with open("input") as file_in:
+    lines = []
+    for line in file_in:
+        lines.append(line)
+
+# parse through lines and increment count if l2 > l1
+
+# print final answer
