1d8fcafe8ae25ce055f73772089ad464f9303f37
aleiadevore <2496@holbertonschool.com>
2021-12-12 18:26:46 -0600

    Whiteboarding day 5

---
+++ Hydrothermal/hydrothermal.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Calculates number of times lines intersect"""
+
+def hydrothermal():
+    """Takes input from text file, plots lines given and
+    prints number of times lines intersect"""
+    # import text files
+
+    # save sets of coordinates
+    # if x1 = x2 plot line at x
+    # if y1 = y2 plot line of y
+    # while plotting, point += 1
+    # create count = 0
+    # iterate through number of points > 1
+
+    # this version uses a matrix. Would it be easier with a dict?
+
+if __name__ == "__main__":
+    hydrothermal()
